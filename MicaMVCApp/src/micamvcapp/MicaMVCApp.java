a48be96de97fd8772f632df25e13238168cfce1d
micamarietta <44626975+micamarietta@users.noreply.github.com>
2021-10-17 18:19:08 -0700

    Created new file for base project
    Created new file MicaMVCProject
    Deleted old project file

--- MicaMVCApp/src/micamvcapp/MicaMVCApp.java
+++ MicaMVCApp/src/micamvcapp/MicaMVCApp.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package mvcappmica;
+package micamvcapp;
 
 /**
  *
  * @author rmari
  */
-public class MvcAppMica {
+public class MicaMVCApp {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // TODO code application logic here
+        System.out.println("Hello!");
     }
     
 }

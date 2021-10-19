f214a8096e5919a77b8fe5e4b251b984e0cafb5b
micamarietta <44626975+micamarietta@users.noreply.github.com>
2021-10-18 17:04:51 -0700

    Added basic beginnings for MVC pattern
    Added model and controller for bands/artists

---
+++ MicaMVCApp/src/micamvcapp/BandController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package micamvcapp;
+
+/**
+ *
+ * @author rmari
+ */
+public class BandController {
+    //controller for each band/artist present
+    
+}
---
+++ MicaMVCApp/src/micamvcapp/Models/BandModel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package micamvcapp.Models;
+
+/**
+ *
+ * @author rmari
+ */
+public class BandModel {
+    //model for band/ artist
+     
+    public String name;
+    public String info;
+    
+}
